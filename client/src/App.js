d4a2baf6630ecfd8c95ddd87906e69a55d49004b
NotCarlos <124255951+cvillaru@users.noreply.github.com>
2023-09-20 01:46:35 +0100

    created update button and modal

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 
+import { Modal, Button } from "react-bootstrap";
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   const rootDB = "http://localhost:5001"
 
@@ -57,8 +61,108 @@ function App() {
   };
 
 
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [updatedEmployee, setUpdatedEmployee] = useState({
+    name: "",
+    age: 0,
+    country: "",
+    position: "",
+    wage: 0,
+  });
+
+
+  const handleShowUpdateModal = () => {
+    setShowUpdateModal(true);
+  };
+  
+  const handleCloseUpdateModal = () => {
+    setShowUpdateModal(false);
+  };
+
+
+  const handleUpdateEmployee = async (id) => {
+    // try {
+    //   // Make an API call to update the employee data with updatedEmployee
+    //   await axios.put(`${rootDB}/update-employee/${employeeId}`, updatedEmployee);
+    //   // Close the modal
+    //   handleCloseUpdateModal();
+    //   // Fetch the updated employee list
+    //   getEmployees();
+    // } catch (error) {
+    //   console.error("ERROR: ", error);
+    // }
+  };
+
+
   return (
     <div className='App'>
+      <Modal 
+        centered
+        show={showUpdateModal}
+        onHide={handleCloseUpdateModal}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Update Employee</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="employeeName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control 
+                type="text"
+                placeholder="Name"
+                defaultValue={employee.name}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="employeeAge">
+              <Form.Label>Age</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Age"
+                defaultValue={employee.age}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="employeeCountry">
+              <Form.Label>Country</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Country"
+                defaultValue={employee.country}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="employeePosition">
+              <Form.Label>Position</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Position"
+                defaultValue={employee.position}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="employeeWage">
+              <Form.Label>Wage</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Wage"
+                defaultValue={employee.wage}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseUpdateModal}>
+            Close
+          </Button>
+          <Button
+            variant="primary"
+            onClick={()=>{handleUpdateEmployee(employee.id)}}
+          >
+            Update Details
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <div className="information">
         <lable>Name: </lable>
         <input type="text" placeholder='Name' onChange={handleChange} name="name"/>
@@ -74,7 +178,6 @@ function App() {
       </div>
 
 
-      <div className='App'>
       {employeeList.map((value, key) => (
         <div className='employee-display-container' key={key}>
           <div className='employee-display-content'>
@@ -85,7 +188,7 @@ function App() {
             <h3>Wage: {value.wage}</h3>
           </div>
           <div className='action-button-container'>
-            <button>
+            <button onClick={handleShowUpdateModal}>
               Update Employee
             </button>
             <button onClick={()=>{handleDeleteEmployee(value.id)}}>
@@ -94,7 +197,6 @@ function App() {
           </div>
         </div>
       ))}
-    </div>
     </div>
     
   );
