5b71bc04ba3a8a83b515341a90dad16581117303
NotCarlos <124255951+cvillaru@users.noreply.github.com>
2024-01-12 11:47:07 +0000

    NOT working version
    don't know why, not working.

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,8 @@ import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
-import Alert from 'react-bootstrap/Alert';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -164,168 +165,193 @@ function App() {
 
   return (
     <div className='App'>
-      <>
+
+
+    <Tabs
+      defaultActiveKey="profile"
+      id="fill-tab-example"
+      className="mb-3"
+      fill
+    >
+      <Tab eventKey="homeTab" title="Home">
+        Tab content for Home
+      </Tab>
+      <Tab eventKey="addEmployeeTab" title="Add Employee">
+
+        <>
         <Modal
           show={showAlertModal}
           onHide={handleCloseAlertModal}
           backdrop="static"
           keyboard={false}
         >
-          <Alert variant="danger">
-            <Alert.Heading>Invalid Input(s)</Alert.Heading>
-            <p>
-              Ensure all inputs are filled in.
-            </p>
-          </Alert>
-          <Modal.Footer>
+          <Modal.Header
+            style={{ backgroundColor: "rgba(255, 68, 68, 0.47)"}}
+          >
+            <Modal.Title>
+              Invalid Input(s)
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body
+            style={{ backgroundColor: "rgba(255, 68, 68, 0.47)"}}
+          >
+            Ensure all inputs are filled in.
+          </Modal.Body>
+          <Modal.Footer 
+            style={{ backgroundColor: "rgba(255, 68, 68, 0.47)"}}
+          >
             <Button variant="primary" onClick={handleCloseAlertModal}>
               Ok
             </Button>
           </Modal.Footer>
         </Modal>
-      </>
+        </>
 
-      <div className="information">
-        <lable>Name: </lable>
-        <input type="text" placeholder='Name' onChange={handleChange} name="name"/>
-        <label>Age: </label>
-        <input type="number" placeholder='Age' onChange={handleChange} name="age"/>
-        <label>Country: </label>
-        <input type="text" placeholder='Country' onChange ={handleChange}name="country"/>
-        <label>Position: </label>
-        <input type="text" placeholder='Position' onChange={handleChange} name="position"/>
-        <label>Wage: </label>
-        <input type="number" placeholder='Wage' onChange={handleChange} name="wage"/>
-        <button onClick={handleAddEmployee}>Add Employee</button>
-      </div>
-
-      <>
-        <Modal
-          show={showUpdateModal}
-          onHide={handleClose}
-          backdrop="static"
-          keyboard={false}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Employee Details</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="employeeName">
-              <Form.Label>Name</Form.Label>
-              <Form.Control 
-                type="text"
-                placeholder="Name"
-                defaultValue={selectedEmployee.name}
-                name="employeeName"
-                onChange={handleUpdateInput}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="employeeAge">
-              <Form.Label>Age</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Age"
-                defaultValue={selectedEmployee.age}
-                name="employeeAge"
-                onChange={handleUpdateInput}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="employeeCountry">
-              <Form.Label>Country</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Country"
-                defaultValue={selectedEmployee.country}
-                name="employeeCountry"
-                onChange={handleUpdateInput}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="employeePosition">
-              <Form.Label>Position</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Position"
-                defaultValue={selectedEmployee.position}
-                name="employeePosition"
-                onChange={handleUpdateInput}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="employeeWage">
-              <Form.Label>Wage</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Wage"
-                defaultValue={selectedEmployee.wage}
-                name="employeeWage"
-                onChange={handleUpdateInput}/>
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Cancel
-            </Button>
-            <Button variant="primary" onClick={()=>{handleUpdateEmployee(selectedEmployee.id)}}>
-              Ok
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-
-      <>
-        <Modal
-          show={showDeleteModal}
-          onHide={handleCloseConfirmDeletion}
-          backdrop="static"
-          keyboard={false}
-          animation={false}
-          centered
-        >
-          <Modal.Header
-            style={{ backgroundColor: "rgba(255, 68, 68, 0.47)"}}
-          >
-            <Modal.Title>WARNING</Modal.Title>
-          </Modal.Header>
-          <Modal.Body
-            style={{ backgroundColor: "rgba(255, 68, 68, 0.47)" }}
-            className="text-center"
-          >
-            About to delete employee.
-            Are you sure?
-          </Modal.Body>
-          <Modal.Footer
-            style={{ backgroundColor: "rgba(255, 68, 68, 0.47)" }}
-          >
-            <Button variant="secondary" onClick={handleCloseConfirmDeletion}>
-              No
-            </Button>
-            <Button variant="danger" onClick={()=>{handleDeleteEmployee(deleteEmployee.id)}}>
-              Yes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-
-      {employeeList.map((value, key) => (
-        <div className='employee-display-container' key={key}>
-          <div className='employee-display-content'>
-            <h3>Name: {value.name} </h3>
-            <h3>Age: {value.age} </h3>
-            <h3>Country: {value.country}</h3>
-            <h3>Position: {value.position}</h3>
-            <h3>Wage: {value.wage}</h3>
-          </div>
-            <div className="d-grid gap-2">
-              <Button variant="primary" onClick={()=>{handleShowEmployeeDetails(value.id)}}>
-                Update Employee Details
-              </Button>
-              <Button variant="danger" onClick={()=>{handleShowConfirmDeletion(value.id)}}>
-                Delete Employee
-              </Button>
-            </div>
+        <div className="information">
+          <lable>Name: </lable>
+          <input type="text" placeholder='Name' onChange={handleChange} name="name"/>
+          <label>Age: </label>
+          <input type="number" placeholder='Age' onChange={handleChange} name="age"/>
+          <label>Country: </label>
+          <input type="text" placeholder='Country' onChange ={handleChange}name="country"/>
+          <label>Position: </label>
+          <input type="text" placeholder='Position' onChange={handleChange} name="position"/>
+          <label>Wage: </label>
+          <input type="number" placeholder='Wage' onChange={handleChange} name="wage"/>
+          <button onClick={handleAddEmployee}>Add Employee</button>
         </div>
-      ))}
+
+        <>
+          <Modal
+            show={showUpdateModal}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Employee Details</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="employeeName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control 
+                  type="text"
+                  placeholder="Name"
+                  defaultValue={selectedEmployee.name}
+                  name="employeeName"
+                  onChange={handleUpdateInput}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="employeeAge">
+                <Form.Label>Age</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Age"
+                  defaultValue={selectedEmployee.age}
+                  name="employeeAge"
+                  onChange={handleUpdateInput}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="employeeCountry">
+                <Form.Label>Country</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Country"
+                  defaultValue={selectedEmployee.country}
+                  name="employeeCountry"
+                  onChange={handleUpdateInput}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="employeePosition">
+                <Form.Label>Position</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Position"
+                  defaultValue={selectedEmployee.position}
+                  name="employeePosition"
+                  onChange={handleUpdateInput}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="employeeWage">
+                <Form.Label>Wage</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Wage"
+                  defaultValue={selectedEmployee.wage}
+                  name="employeeWage"
+                  onChange={handleUpdateInput}/>
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Cancel
+              </Button>
+              <Button variant="primary" onClick={()=>{handleUpdateEmployee(selectedEmployee.id)}}>
+                Ok
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </>
+
+        <>
+          <Modal
+            show={showDeleteModal}
+            onHide={handleCloseConfirmDeletion}
+            backdrop="static"
+            keyboard={false}
+            animation={false}
+            centered
+          >
+            <Modal.Header
+              style={{ backgroundColor: "rgba(255, 68, 68, 0.47)"}}
+            >
+              <Modal.Title>WARNING</Modal.Title>
+            </Modal.Header>
+            <Modal.Body
+              style={{ backgroundColor: "rgba(255, 68, 68, 0.47)" }}
+              className="text-center"
+            >
+              About to delete employee.
+              Are you sure?
+            </Modal.Body>
+            <Modal.Footer
+              style={{ backgroundColor: "rgba(255, 68, 68, 0.47)" }}
+            >
+              <Button variant="secondary" onClick={handleCloseConfirmDeletion}>
+                No
+              </Button>
+              <Button variant="danger" onClick={()=>{handleDeleteEmployee(deleteEmployee.id)}}>
+                Yes
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </>
+
+        {employeeList.map((value, key) => (
+          <div className='employee-display-container' key={key}>
+            <div className='employee-display-content'>
+              <h3>Name: {value.name} </h3>
+              <h3>Age: {value.age} </h3>
+              <h3>Country: {value.country}</h3>
+              <h3>Position: {value.position}</h3>
+              <h3>Wage: {value.wage}</h3>
+            </div>
+              <div className="d-grid gap-2">
+                <Button variant="primary" onClick={()=>{handleShowEmployeeDetails(value.id)}}>
+                  Update Employee Details
+                </Button>
+                <Button variant="danger" onClick={()=>{handleShowConfirmDeletion(value.id)}}>
+                  Delete Employee
+                </Button>
+              </div>
+          </div>
+        ))}
+        
+      </Tab>
+    </Tabs>
+
     </div>
     
   );
